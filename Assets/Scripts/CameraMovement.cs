8e285aa4be3634d91a879004ebe8db2f855e5005
serhatserter <serhatserter02@outlook.com>
2021-01-30 12:43:10 +0300

    player movement added

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float CamDistanceY = 5f;
+    public float CamDistanceZ = 8f;
+    private Vector3 camPosition;
+    private float camPositionY;
+    private float camPositionZ;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        camPositionY = GameManager.Instance.Player.transform.position.y + CamDistanceY;
+        camPositionZ = GameManager.Instance.Player.transform.position.z - CamDistanceZ;
+        camPosition = new Vector3(transform.position.x, camPositionY, camPositionZ);
+
+        transform.position = Vector3.Lerp(transform.position, camPosition, 10f * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float playerMoveZSpeed;
+
+    private Vector3 screenPosition;
+    private Vector2 worldPosition;
+    private Vector3 PlayerSidePosition;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerMoveZSpeed = GameManager.Instance.PlayerMoveZSpeed;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            PlayerForwardMovement();
+            PlayerSideMovement();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            PlayerStopMovement();
+        }
+    }
+
+    void PlayerSideMovement()
+    {
+        screenPosition = Input.mousePosition;
+        screenPosition.z = 20f;
+        worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        PlayerSidePosition = new Vector3(worldPosition.x, transform.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, PlayerSidePosition, 10f * Time.deltaTime);
+    }
+
+    void PlayerForwardMovement()
+    {
+        playerRb.velocity = new Vector3(playerRb.velocity.x, playerRb.velocity.y, playerMoveZSpeed);
+    }
+
+    void PlayerStopMovement()
+    {
+        playerRb.velocity = new Vector3(playerRb.velocity.x, playerRb.velocity.y, 0);
+    }
+
+
+}
